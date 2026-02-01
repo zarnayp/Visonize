6d791b0dca4877c4e882ee9021083608db1cc9d3
Zarnay <39445270+zarnayp@users.noreply.github.com>
2026-02-01 14:59:29 -0800

    viewer and imaging split

--- Imaging/Domain/src/Service/Framework/DispatcherProxy.cs
+++ Imaging/Domain/src/Service/Framework/DispatcherProxy.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-[assembly: InternalsVisibleTo("DupploPulse.UsImaging.Domain.Tests")]
+[assembly: InternalsVisibleTo("Visonize.UsImaging.Domain.Tests")]
 
 namespace DupploPulse.UsImaging.Domain.Service.Framework
 {
